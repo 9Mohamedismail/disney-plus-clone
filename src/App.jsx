7bacfba9a0aeca1e18fb13b7ff2fa79daff60d6f
9Mohamedismail <9Mohamedismail@gmail.com>
2024-04-14 22:59:23 -0400

    Added Home + ImgSlider + Viewers component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header";
-import Home from "./pages/Home";
+import Home from "./components/Home";
+import "./App.css";
 
 function App() {
   return (
