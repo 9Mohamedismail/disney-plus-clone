9f20c2aa1368700a9607c62d0a466bfa2985cc4f
9Mohamedismail <9Mohamedismail@gmail.com>
2024-04-15 21:10:29 -0400

    Home page + Detail Page complete. Router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Header from "./components/Header";
-import Home from "./components/Home";
+import Home from "./pages/Home";
 import "./App.css";
+import Detail from "./components/Detail";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
-      <Home />
-    </>
+      <Routes>
+        <Route path="detail" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
 
